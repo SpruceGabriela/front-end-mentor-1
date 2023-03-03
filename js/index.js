d793c2d47c284a2d7f814a3c391f2b629b1ddf50
Gabriela Pinheiro <gabrielapinheiro129@gmail.com>
2023-03-03 00:19:01 -0300

    implement logic to show the feedback modal

--- js/index.js
+++ js/index.js
@@ -1,14 +1,36 @@
+let checked;
+const ratingContainer = document.querySelector(".rating_options");
+const submitButton = document.querySelector(".rating_submit");
+const feedbackModal = document.querySelector(".rating_feedback");
 const renderCheckbox = (number) => `<div>
-  <input class="checkbox" type="radio" name="checkbox" id="${number}" />
+  <input 
+    class="checkbox" 
+    type="checkbox" 
+    id="${number}" 
+    value="${number}"
+    onclick="handleCheckboxClick(this)"
+  />
   <label class="rating_option" for="${number}">${number}</label>
 </div>
 `;
-const ratingContainer = document.querySelector(".rating_options");
-
-console.log(ratingContainer)
 
 if(ratingContainer) {
   for(let i = 1; i <= 5; i++) {
     ratingContainer.insertAdjacentHTML('beforeend', renderCheckbox(i))
   }
 }
+
+const handleCheckboxClick = (el) => {
+  const allCheckboxes = document.querySelectorAll(".checkbox");
+  allCheckboxes.forEach((item) => item.checked = false);
+  el.checked = true;
+  checked = el.getAttribute("id");
+}
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  setTimeout(() => {
+    feedbackModal.classList.add("active")
+  }, 5000)
+})
